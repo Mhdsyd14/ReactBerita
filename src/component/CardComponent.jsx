5780adaa7e46b6664957ae1692dc5df0ca1dd18e
DESKTOP-C2OUG3H\MhdSyd014 <irsyadmuhammad954@gmail.com>
2024-05-11 16:06:55 +0700

    dikit

--- src/component/CardComponent.jsx
+++ src/component/CardComponent.jsx
@@ -3,6 +3,10 @@ import axios from "axios";
 import { ColorRing } from "react-loader-spinner";
 
 export class CardComponent extends Component {
+  formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString("en-US", options);
+  };
   render() {
     return (
       <>
@@ -17,7 +21,9 @@ export class CardComponent extends Component {
               <div className="card-body">
                 <h5 className="card-title">{article.title}</h5>
                 <p className="card-text">{article.description}</p>
-                <p className="card-text">{article.publishedAt}</p>
+                <p className="card-text">
+                  {this.formatDate(article.publishedAt)}
+                </p>
                 <a href={article.url} className="btn btn-primary">
                   Read more
                 </a>
